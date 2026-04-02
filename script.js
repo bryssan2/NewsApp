3437a81d5a4cb6632ea3ba0b87224f46e34db8a9
Brice <bryssan2006@gmail.com>
2026-04-02 13:40:26 +0000

    création de la fonction searchTerm() qui vérifie si l'utilisateur a entré un valeur dans la barre de recherche

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
+let input = document.getElementById("search");
+let articleImg = document.getElementById("image");
+let CardTitle = document.getElementById("card-title");
+let cardDesc = document.getElementById("card-desc");
+let date = document.getElementById("Date");
+let hours = document.getElementById("Hours");
+let category = document.getElementById("category");
+let Icon = document.getElementById("icon");
 let APIKey = "pub_5bc4392416d24d448263cf233ef1504b";
-async function News() {
-  const res = await fetch(
-    ` https://newsdata.io/api/1/latest?apikey=${APIKey}&q=ia&language=fr`,
-  );
-  if (!res.ok) throw new Error("Request failed");
-  const data = await res.json();
-  console.log(data);
+
+function searchTerm() {
+  if (input.value) {
+    let encodedInput = encodeURIComponent(input.value.trim());
+    console.log(encodedInput);
+    return true;
+  } else {
+    console.log(false);
+    return false;
+  }
 }
 
-News();
+// async function fetchData() {
+//   const res = await fetch(
+//     ` https://newsdata.io/api/1/latest?apikey=${APIKey}&q=${encodedInput}&language=fr`,
+//   );
+//   if (!res.ok) throw new Error("Request failed");
+//   const data = await res.json();
+//   return data
+// }
